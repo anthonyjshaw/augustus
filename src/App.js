65090cb2ec5f59872919816b8769c83b704b9fb4
Anthony Shaw <anthonyshaw@sceaga.com>
2021-10-29 21:36:16 +0100

    Added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Router } from 'react-router';
+
+const App = () => {
+	return (
+		<div>
+			<h1>Hello</h1>
+		</div>
+	)
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const Footer = () => {
+
+	return (
+		<div>
+			<div className="container flex">
+				<div className='footer-logo'></div>
+				<div className='nav-links'>
+					
+				</div>
+			</div>
+
+		</div>
+	);
+}
+
+export default Footer;
